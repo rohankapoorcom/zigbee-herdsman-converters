f95bb582be2d0ab82081e9c5f21107b19b0ae661
Andy <andrei_lazarov@yahoo.com>
2026-04-30 18:42:13 +0300

    fix(detect): Detect HOBEIAN ZG-227Z as HOBEIAN ZG-227Z (#12061)

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -13093,7 +13093,17 @@ export const definitions: DefinitionWithExtend[] = [
             e.battery(),
         ],
         whiteLabel: [
-            tuya.whitelabel("HOBEIAN", "ZG-227Z", "Temperature and humidity sensor", ["_TZE200_a8sdabtg", "_TZE200_vs0skpuc", "_TZE200_ehhrv2e3"]),
+            {
+                model: "ZG-227Z",
+                vendor: "HOBEIAN",
+                description: "Temperature & humidity sensor",
+                fingerprint: [
+                    {modelID: "ZG-227Z"},
+                    {manufacturerName: "_TZE200_a8sdabtg"},
+                    {manufacturerName: "_TZE200_vs0skpuc"},
+                    {manufacturerName: "_TZE200_ehhrv2e3"},
+                ],
+            },
             tuya.whitelabel("KOJIMA", "KOJIMA-THS-ZG-LCD", "Temperature and humidity sensor", ["_TZE200_dikkika5"]),
         ],
         meta: {
